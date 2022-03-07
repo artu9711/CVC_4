5b8c17f22f724bd6d69433b74acd9c567061c148
arturodcv <59840729+arturodcv@users.noreply.github.com>
2022-03-07 16:24:20 +0100

    Update algorithm.py

--- algorithm.py
+++ algorithm.py
@@ -6,24 +6,18 @@ import time
 from glob import glob
 import shutil
 import sys
+import ast
 
-with open('execution_time.txt', 'r') as f:
-    last_time_running = int(f.read())
-if last_time_running > 20.0:
-    update_parameters()
-
-last_run_file = max(last_params_path+'/*', key=os.path.getctime)
-with open(filename, 'rb') as f:
-     spikes_last_running = pickle.load(f)['exc_spikes_from']
-if spikes_last_running > 98735:
-    update_parameters()
-
-path, dirs, files = next(os.walk(path)); len_runnings = len(files)
-if len_runnings < 8: 
-    quit()
 
 best_params_path = 'best_params'
 last_params_path = 'last_params'
+
+####### funciones
+
+def get_score(means_spikes,means_gammas):
+    score_spikes = scipy.stats.spearmanr(means_spikes,[2.5,3,3.5,4,4.5,5,5.5,6])
+    score_gammas = scipy.stats.spearmanr(means_gammas,[2.5,3,3.5,4,4.5,5,5.5,6])  
+    return score_gammas - score_spikes
 
 def get_df(path):
     keys = ['image_name','exc_spikes_from','inh_spikes_from', 'node_exc', 'gamma_power_exc','node_tot','gamma_power_tot', 'seed']
@@ -46,6 +40,79 @@ def get_score(means_spikes,means_gammas):
     score_gammas = scipy.stats.spearmanr(means_gammas,[2.5,3,3.5,4,4.5,5,5.5,6])  
     return score_gammas - score_spikes
 
+def move_contents(origin_path_name,goal_path_name):
+    for filename in os.listdir(origin_path_name):
+        file_path = os.path.join(origin_path_name, filename)
+        shutil.move(file_path + ".foo",os.path.join(goal_path_name, filename) + ".foo")
+        
+def save_parameters_to_txt(parameters_dict, filename):
+    with open(filename, 'w') as f:
+        print(parameters_dict, file=f)
+        
+def read_parameters_from_txt(filename):
+    file = open(filename, "r")
+    contents = file.read()
+    dictionary = ast.literal_eval(contents)
+    file.close()
+    return dictionary
+  
+def update_parameters(old_parameters_dict):
+    new_parameters_dict = {}
+    
+    new_parameters_dict['weight_large_range_exc_exc'] = old_parameters_dict['weight_large_range_exc_exc'] + (-1)**np.random.randint(0,2) * np.random.uniform(0,0.001)
+    while new_parameters_dict['weight_large_range_exc_exc'] < 0.0 or new_parameters_dict['weight_large_range_exc_exc'] > 0.05:
+        new_parameters_dict['weight_large_range_exc_exc'] = old_parameters_dict['weight_large_range_exc_exc'] + (-1)**np.random.randint(0,2) * np.random.uniform(0,0.001)
+     
+    new_parameters_dict['weight_large_range_exc_inh'] = old_parameters_dict['weight_large_range_exc_inh'] + (-1)**np.random.randint(0,2) * np.random.uniform(0,0.005)
+    while new_parameters_dict['weight_large_range_exc_inh'] < 0.0 or new_parameters_dict['weight_large_range_exc_inh'] > 0.1:
+        new_parameters_dict['weight_large_range_exc_inh'] = old_parameters_dict['weight_large_range_exc_inh'] + (-1)**np.random.randint(0,2) * np.random.uniform(0,0.005)
+    
+    new_parameters_dict['weight_inh_exc_'] = old_parameters_dict['weight_inh_exc_'] + (-1)**np.random.randint(0,2) * np.random.uniform(0,0.1)
+    while new_parameters_dict['weight_inh_exc_'] < 0.01 or new_parameters_dict['weight_inh_exc_'] > 1.0:
+        new_parameters_dict['weight_inh_exc_'] = old_parameters_dict['weight_inh_exc_'] + (-1)**np.random.randint(0,2) * np.random.uniform(0,0.1)
+    
+    new_parameters_dict['weight_inh_inh_'] = old_parameters_dict['weight_inh_inh_'] + (-1)**np.random.randint(0,2) * np.random.uniform(0,0.1)
+    while new_parameters_dict['weight_inh_inh_'] < 0.01 or new_parameters_dict['weight_inh_inh_'] > 1.0:
+        new_parameters_dict['weight_inh_inh_'] = old_parameters_dict['weight_inh_inh_'] + (-1)**np.random.randint(0,2) * np.random.uniform(0,0.1)
+    
+    new_parameters_dict['weight_exc_exc'] = old_parameters_dict['weight_exc_exc'] + (-1)**np.random.randint(0,2) * np.random.uniform(0,0.1)
+    while new_parameters_dict['weight_exc_exc'] < 0.01 or new_parameters_dict['weight_exc_exc'] > 1.0:
+        new_parameters_dict['weight_exc_exc'] = old_parameters_dict['weight_exc_exc'] + (-1)**np.random.randint(0,2) * np.random.uniform(0,0.1)
+    
+    new_parameters_dict['weight_exc_inh'] = old_parameters_dict['weight_exc_inh'] + (-1)**np.random.randint(0,2) * np.random.uniform(0,0.1)
+    while new_parameters_dict['weight_exc_inh'] < 0.01 or new_parameters_dict['weight_exc_inh'] > 1.0:
+        new_parameters_dict['weight_exc_inh'] = old_parameters_dict['weight_exc_inh'] + (-1)**np.random.randint(0,2) * np.random.uniform(0,0.1)
+    
+    new_parameters_dict['input_weight_poiss_inh'] = old_parameters_dict['input_weight_poiss_inh'] + (-1)**np.random.randint(0,2) * np.random.uniform(0,0.1)
+    while new_parameters_dict['input_weight_poiss_inh'] < 0.1 or new_parameters_dict['input_weight_poiss_inh'] > 1.0:
+        new_parameters_dict['input_weight_poiss_inh'] = old_parameters_dict['input_weight_poiss_inh'] + (-1)**np.random.randint(0,2) * np.random.uniform(0,0.1)
+    
+    return new_parameters_dict
+
+####### algoritmo
+best_parameters = read_parameters_from_txt(best_params_path + '/best_parameters.txt')
+
+with open('execution_time.txt', 'r') as f:
+    last_execution_time = int(f.read())
+if last_execution_time > 20.0:
+    remove_contents(last_params_path)
+    new_parameters = update_parameters(best_parameters)
+    quit()
+
+last_run_file = max(last_params_path+'/*', key=os.path.getctime)
+with open(filename, 'rb') as f:
+     last_spikes = pickle.load(f)['exc_spikes_from']
+if last_spikes > 98735:
+    remove_contents(last_params_path)
+    new_parameters = update_parameters(best_parameters)
+    quit()
+
+path, dirs, files = next(os.walk(path)); len_runnings = len(files)
+if len_runnings < 8: 
+    quit()
+
+
+
 df_best_params = get_df(best_params_path)
 df_last_params = get_df(last_params_path)
 
@@ -53,52 +120,14 @@ best_score = get_score(df_best_params['exc_spikes_from'],df_best_params['gamma_p
 last_score = get_score(df_last_params['exc_spikes_from'],df_last_params['gamma_power_exc'])
 
 if last_score > best_score:
-    save_best_parameters()
     remove_contents(best_params_path)
+    save_parameters_to_txt(last_parameters, best_params_path + '/best_parameters.txt')
     move_contents(last_params_path,best_params_path)
 
 
-def move_contents(origin_path_name,goal_path_name):
-    for filename in os.listdir(origin_path_name):
-        file_path = os.path.join(origin_path_name, filename)
-        shutil.move(file_path + ".foo",os.path.join(goal_path_name, filename) + ".foo")
+new_parameters = update_parameters(best_parameters)
 
-update_parameters()
 
-def save_best_parameters(parameters_dict):
-    save_dict(parameters_dict,'best_parameters')
-  
-def update_parameters(old_parameters_dict):
-    new_parameters_dict = {}
-    new_parameters_dict['weight_large_range_exc_exc'] = old_parameters_dict['weight_large_range_exc_exc'] + (-1)**np.random.randint(0,2) * 
-    while new_parameters_dict['weight_large_range_exc_exc'] < 0.0 or new_parameters_dict['weight_large_range_exc_exc'] > 0.05:
-        new_parameters_dict['weight_large_range_exc_exc'] = old_parameters_dict['weight_large_range_exc_exc'] + (-1)**np.random.randint(0,2) * 
-     
-    new_parameters_dict['weight_large_range_exc_inh'] = old_parameters_dict['weight_large_range_exc_inh'] + (-1)**np.random.randint(0,2) * 
-    while new_parameters_dict['weight_large_range_exc_inh'] < 0.0 or new_parameters_dict['weight_large_range_exc_inh'] > 0.1:
-        new_parameters_dict['weight_large_range_exc_inh'] = old_parameters_dict['weight_large_range_exc_inh'] + (-1)**np.random.randint(0,2) * 
-    
-    new_parameters_dict['weight_inh_exc_'] = old_parameters_dict['weight_inh_exc_'] + (-1)**np.random.randint(0,2) * 
-    while new_parameters_dict['weight_inh_exc_'] < 0.0 or new_parameters_dict['weight_inh_exc_'] > 0.1:
-        new_parameters_dict['weight_inh_exc_'] = old_parameters_dict['weight_inh_exc_'] + (-1)**np.random.randint(0,2) * 
-    
-    new_parameters_dict['weight_inh_inh_'] = old_parameters_dict['weight_inh_inh_'] + (-1)**np.random.randint(0,2) * 
-    while new_parameters_dict['weight_inh_exc_'] < 0.0 or new_parameters_dict['weight_inh_exc_'] > 0.1:
-        new_parameters_dict['weight_inh_exc_'] = old_parameters_dict['weight_inh_exc_'] + (-1)**np.random.randint(0,2) * 
-    
-    new_parameters_dict['weight_exc_exc'] = old_parameters_dict['weight_exc_exc'] + (-1)**np.random.randint(0,2) * 
-    while new_parameters_dict['weight_inh_exc_'] < 0.0 or new_parameters_dict['weight_inh_exc_'] > 0.1:
-        new_parameters_dict['weight_inh_exc_'] = old_parameters_dict['weight_inh_exc_'] + (-1)**np.random.randint(0,2) * 
-    
-    new_parameters_dict['weight_exc_inh'] = old_parameters_dict['weight_exc_inh'] + (-1)**np.random.randint(0,2) * 
-    while new_parameters_dict['weight_inh_exc_'] < 0.0 or new_parameters_dict['weight_inh_exc_'] > 0.1:
-        new_parameters_dict['weight_inh_exc_'] = old_parameters_dict['weight_inh_exc_'] + (-1)**np.random.randint(0,2) * 
-    
-    new_parameters_dict['input_weight_poiss_inh'] = old_parameters_dict['input_weight_poiss_inh'] + (-1)**np.random.randint(0,2) * 
-    while new_parameters_dict['weight_inh_exc_'] < 0.0 or new_parameters_dict['weight_inh_exc_'] > 0.1:
-        new_parameters_dict['weight_inh_exc_'] = old_parameters_dict['weight_inh_exc_'] + (-1)**np.random.randint(0,2) * 
-    
-    return new_parameters_dict
     
     
    
